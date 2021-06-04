28f85804c85f8b2bf0f8caec10009dbe551476c8
DevHyup <02ydh@naver.com>
2021-06-04 23:36:07 +0900

    good

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ document.getElementById('submit').onclick = function () {
   var pass = document.getElementById('pw').value;
   if (ID === '1234' && pass === '1234') {
     alert('로그인 성공!');
-    location.href = './chart.html';
+    location.href = '/chart.html';
   } else {
     alert('로그인 실패!');
   }
